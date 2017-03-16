b6cd9aec528bae25246d1d0443c17739f3fafd0c
Ku <deylonku@gmail.com>
2017-03-16 14:15:45 +0800

    Implemented design

---
+++ app/src/main/java/com/example/ku/bustrack/RequestLocationActivity.java
@@ -0,0 +1,13 @@
+package com.example.ku.bustrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_location);
+    }
+}
